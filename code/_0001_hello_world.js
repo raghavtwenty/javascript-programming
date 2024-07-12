abc8a5a55e406288ba09b3482ea992033f8be3ba
raghavtwenty <raghavtwenty@gmail.com>
2024-07-12 21:13:28 +0530

    added hello world

---
+++ code/_0001_hello_world.js
@@ -0,0 +1,10 @@
+/*
+Filename: _0001_hello_world.js
+Title: First program in javascript
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: July 12, 2024 | Last Updated: July 12, 2024
+Language: Javascript
+*/
+
+// Prints
+console.log("Hello, World!");
