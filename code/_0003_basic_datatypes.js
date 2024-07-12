e8be4d60e3fdf2ae72ab491b9ebcdfe9db3d0420
raghavtwenty <raghavtwenty@gmail.com>
2024-07-12 21:13:55 +0530

    added basic datatypes

---
+++ code/_0003_basic_datatypes.js
@@ -0,0 +1,50 @@
+/*
+Filename: _0003_basic_datatypes.js
+Title: Basic datatypes in javascript
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: July 12, 2024 | Last Updated: July 12, 2024
+Language: Javascript
+*/
+
+// Number
+let integerNumber = 42; // Integer
+console.log(integerNumber);      
+
+let floatNumber = 3.14; // Floating-point number
+console.log(floatNumber);        
+
+
+// String
+let singleQuoteString = 'Hello, World!'; // Using single quotes
+console.log(singleQuoteString);  
+
+let doubleQuoteString = "Hello, World!"; // Using double quotes
+console.log(doubleQuoteString);  
+
+
+// Boolean
+let isJavaScriptFun = true; // Boolean true
+console.log(isJavaScriptFun);   
+
+let isJavaScriptHard = false; // Boolean false
+console.log(isJavaScriptHard);   
+
+
+// Null
+let emptyValue = null; // Null
+console.log(emptyValue);   
+
+
+// Undefined
+let notAssigned; // Undefined
+console.log(notAssigned); 
+
+
+// BigInt
+let bigIntNumber = 9007199254740991n; // BigInt
+console.log(bigIntNumber);      
+
+
+// Symbol
+let uniqueId = Symbol('id'); // Unique identifier
+console.log(uniqueId);   
