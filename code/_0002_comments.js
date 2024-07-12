9f4e1b3a7b046b55150641fdb46ed52eb5c316bf
raghavtwenty <raghavtwenty@gmail.com>
2024-07-12 21:13:40 +0530

    added comments

---
+++ code/_0002_comments.js
@@ -0,0 +1,19 @@
+/*
+Filename: _0002_comments.js
+Title: Comments in javascript
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: July 12, 2024 | Last Updated: July 12, 2024
+Language: Javascript
+*/
+
+/*
+Multi-line comment at the top of the file
+is called docstring
+*/
+
+/*
+Multi-line comment
+2nd line
+*/
+
+// Single line comment
