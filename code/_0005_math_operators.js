c2dd7bffc88e048cccdf066819f126d0b2ac4255
raghavtwenty <raghavtwenty@gmail.com>
2024-07-13 21:39:01 +0530

    added math operators

---
+++ code/_0005_math_operators.js
@@ -0,0 +1,50 @@
+/*
+Filename: _0005_math_operators.js
+Title: Mathematical operators in javascript
+Author: Raghava | GitHub: @raghavtwenty
+Date Created: July 13, 2024 | Last Updated: July 13, 2024
+Language: Javascript
+*/
+
+// Addition
+let add = 5 + 3; 
+console.log("5 + 3 =", add);
+
+// Subtraction
+let subtract = 10 - 4; 
+console.log("10 - 4 =", subtract);
+
+// Multiplication
+let multiply = 7 * 6; 
+console.log("7 * 6 =", multiply);
+
+// Division
+let divide = 20 / 5; 
+console.log("20 / 5 =", divide);
+
+// Modulus (remainder)
+let modulus = 14 % 5; 
+console.log("14 % 5 =", modulus);
+
+// Exponentiation 
+let exponentiation = 2 ** 3; 
+console.log("2 ** 3 =", exponentiation);
+
+// Increment
+let increment = 5;
+increment++; 
+console.log("Increment 5 by 1 =", increment);
+
+// Decrement
+let decrement = 5;
+decrement--; 
+console.log("Decrement 5 by 1 =", decrement);
+
+// Unary negation
+let unaryNegation = -5; 
+console.log("Unary negation of 5 =", unaryNegation);
+
+// Unary plus (attempts to convert the operand to a number)
+let unaryPlus = +"5"; 
+console.log('Unary plus on "5" =', unaryPlus);
+
